e041d7786de29aca5282c92876767ea9996cacd3
TheViero <128616582+TheViero@users.noreply.github.com>
2023-04-11 16:23:40 +0200

    sadasd

---
+++ Легковаговик/Легковаговик/Class1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Легковаговик
+{
+    internal class Class1
+    {
+        // Типы облаков
+        public enum CloudType
+        {
+            small,
+            midle,
+            big
+        }
+
+        // Картинки для облаков
+        public static class CloudImageFactory
+        {
+            private static Dictionary<CloudType, Image> _cloudImages;
+
+            static CloudImageFactory()
+            {
+                _cloudImages = new Dictionary<CloudType, Image>();
+
+                // Загрузка картинок облаков
+                _cloudImages[CloudType.small] = Image.FromFile("\"D:\\Легковаговик\\64546_8414.jpg\"");
+                _cloudImages[CloudType.midle] = Image.FromFile("\"D:\\Легковаговик\\134353168712334_2420.jpg\"");
+                _cloudImages[CloudType.big] = Image.FromFile("\"D:\\Легковаговик\\images.jpg\"");
+            }
+
+            public static Image GetCloudImage(CloudType type)
+            {
+                return _cloudImages[type];
+            }
+        }
+
+        // Облако - легковес
+        public class Cloud
+        {
+            public CloudType Type { get; set; }
+            public Image Image { get; set; }
+            public Point Position { get; set; }
+
+            public Cloud(CloudType type, Point position)
+            {
+                Type = type;
+                Image = CloudImageFactory.GetCloudImage(type);
+                Position = position;
+            }
+
+            public void Draw(Graphics graphics)
+            {
+                graphics.DrawImage(Image, Position);
+            }
+        }
+
+        // Менеджер облаков
+        public class CloudManager
+        {
+            private List<Cloud> _clouds;
+
+            public CloudManager()
+            {
+                _clouds = new List<Cloud>();
+            }
+
+            public void AddCloud(CloudType type, Point position)
+            {
+                Cloud cloud = new Cloud(type, position);
+                _clouds.Add(cloud);
+            }
+
+            public void Draw(Graphics graphics)
+            {
+                foreach (Cloud cloud in _clouds)
+                {
+                    cloud.Draw(graphics);
+                }
+            }
+            public void Update(int deltaX, int deltaY)
+            {
+                foreach (Cloud cloud in _clouds)
+                {
+                    // Например, перемещаем облака по прямой
+                    cloud.Position = new Point(cloud.Position.X + deltaX, cloud.Position.Y + deltaY);
+                }
+            }
+        }
+    }
+}
---
+++ Легковаговик/Легковаговик/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static Легковаговик.Class1;
+
+namespace Легковаговик
+{
+    public partial class Form1 : Form
+    {
+        private CloudManager _cloudManager;
+        private Timer _timer;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Создаем менеджер облаков
+            _cloudManager = new CloudManager();
+
+            // Создаем таймер для обновления экрана
+            _timer = new Timer();
+            _timer.Interval = 50;
+            _timer.Tick += timer1_Tick;
+            _timer.Start();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            // Рисуем облака на экране
+            _cloudManager.Draw(e.Graphics);
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            // Добавляем новое облако по клику мыши
+            CloudType type = CloudType.small; // Тип облака, можно задать любой
+            Point position = e.Location; // Позиция облака
+            _cloudManager.AddCloud(type, position);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Обновляем положение облаков
+            _cloudManager.Update(10, 0);
+
+            // Перерисовываем экран
+            pictureBox1.Invalidate();
+        }
+    }
+}
